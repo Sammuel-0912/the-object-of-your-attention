1ed87d99af475b40c3394d8c5cb5055557a100f5
Sammuel-0912 <111262769+Sammuel-0912@users.noreply.github.com>
2024-01-09 10:04:44 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
+using System;
+
+namespace DatingProfile
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Profile sam = new Profile("Sam Drakkila",30,"New york","USA","he/him") ; 
+       Profile jack = new Profile("Jack Drakkila",35) ; 
+      Console.WriteLine(jack.ViewProfile());
+      sam.SetHobbies(new string[] {"listening to audiobooks and podcasts","playing rec sports like bowling and kickball","writing a speculative fiction novel","reading advice columns"} ) ;
+    }
+  }
+}
 
